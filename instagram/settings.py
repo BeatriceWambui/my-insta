f2b80fb18f5f503f4d57f4baecaf37829d1ac06c
Wachira Beatrice <BeatriceWambui>
2019-10-11 14:54:41 +0300

    add insta-app and bootatrap to install apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'insta.apps.InstaConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
