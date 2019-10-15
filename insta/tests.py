60c58f7d42ecc6c59c4f652e47c44d27517419fc
Wachira Beatrice <BeatriceWambui>
2019-10-15 09:25:11 +0300

    create search and profile url and add the media root

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+# from .models import Image,Profile
+# # Create your tests here.
 
-# Create your tests here.
+# class ImageTestClass(TestCase):
+#     def setUp(self):
+#         self.lucy=Image(first_name='Lucy', seco)
