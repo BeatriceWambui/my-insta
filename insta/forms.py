1e26aa429ccf1319093659d7a84fe0078d3892c0
Wachira Beatrice <BeatriceWambui>
2019-10-15 12:13:34 +0300

    create a comment form

--- insta/forms.py
+++ insta/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from .models import CommentFormRecipient
 
 class InstaForm(forms.Form):
     first_name = forms.CharField(label='First Name',max_length=30)
     second_name = forms.CharField(label='Last Name',max_length=30)
     email = forms.EmailField(label='Email')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CommentFormRecipient
+        fields=('comment',)
